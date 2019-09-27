92eaed06295241023eec7c3fb79f413d78946916
max <mkondzera@talend.com>
2019-09-27 13:10:55 +0300

    implement maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,49 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// const pipe = (...fns) => x => fns.reduce((y, f) => f(y), x);
+// const createCharsObject = str => str
+//     .split('')
+//     .reduce((acc, curr) => {
+//         if (!acc[curr]) acc[curr] = 0;
+//         acc[curr]++;
+//         return acc;
+//     }, {});
+
+// const findMaxObjProp = obj => {
+//     let resEntry = [0, -Infinity];
+
+//     Object.entries(obj).forEach(entry => {
+//         if (entry[1] >= resEntry[1]) resEntry = entry;
+//     });
+
+//     return resEntry[0];
+// }
+
+// const maxChar = str => pipe(
+//     createCharsObject,
+//     findMaxObjProp,
+// )(str);
+
+function maxChar(str) {
+    const charMap = {};
+    let max = -Infinity;
+    let maxChar = '';
+
+    for (let char of str) {
+        charMap[char] = charMap[char] + 1 || 1;
+    }
+
+    for (let char in charMap) {
+        if (charMap[char] > max) {
+            max = charMap[char];
+            maxChar = char;
+        }
+    }
+
+    return maxChar;
+}
+
+maxChar('Hello There!');
 
 module.exports = maxChar;
