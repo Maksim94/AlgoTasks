2a9a5f9c983a682c3999546f73dbaeec306eaa7c
max <mkondzera@talend.com>
2019-10-02 16:14:54 +0300

    implement steps algorithm

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,47 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// Recursive solution
+function steps(n, row = 0, stair = '') {
+    if (n === row) {
+        return;
+    }
+
+    if (n === stair.length) {
+        console.log(stair);
+        return steps(n, row + 1);
+    }
+
+    stair += stair.length <= row ? '#' : ' ';
+
+    steps(n, row, stair);
+}
 
 module.exports = steps;
+
+
+// Optimized iterative solution O(n)
+// const steps = n => {
+//     const symbol = '#';
+
+//     for(let i = 1; i <= n; i++) {
+//         console.log(`${symbol.repeat(i)}`.padEnd(n));
+//     }
+// }
+
+// Common iterative solution O(n^2)
+// function steps(n) {
+//     for(let row = 0; row < n; row++) {
+//         let stair = '';
+
+//         for (let column = 0; column < n; column++) {
+//             if (column <= row) {
+//                 stair += '#';
+//             } else {
+//                 stair += ' ';
+//             }
+//         }
+
+//         console.log(stair);
+//     }
+// }
