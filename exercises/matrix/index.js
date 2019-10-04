cff4b03381bfeb21b8f22e75cad5b23c0e985533
max <mkondzera@talend.com>
2019-10-04 15:05:38 +0300

    implement matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,42 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+    const results = Array.from({ length: n }, () => []);
+
+    let count = 1;
+    let startRow = 0;
+    let startColumn = 0;
+    let endRow = n - 1;
+    let endColumn = n - 1;
+
+    while(startRow <= endRow && startColumn <= endColumn) {
+        // Top row
+       for (let i = startColumn; i <= endColumn; i++) {
+           results[startRow][i] = count++; 
+       }
+       startRow++;
+
+       // Right column
+       for(let i = startRow; i <= endRow; i++) {
+           results[i][endColumn] = count++;
+       }
+       endColumn--;
+
+       // Bottom row
+       for(let i = endColumn; i >= startColumn; i--) {
+           results[endRow][i] = count++;
+       }
+       endRow--;
+
+       // Left column
+       for(let i = endRow; i >= startRow; i--) {
+           results[i][startColumn] = count++;
+       }
+       startColumn++;
+    }
+
+    return results;
+}
 
 module.exports = matrix;
