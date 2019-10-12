c78a02bd522066b97781256814ee62ea1d134eb9
maxim <maxim.kondzera@gmail.com>
2019-10-12 12:18:39 +0300

    add levelwidth algo implementation

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,25 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    const symbol = 's';
+    const counters = [0];
+    const array = [root, symbol];
+
+    while(array.length > 1) {
+        const node = array.shift();
+
+        if (node === symbol) {
+            counters.push(0);
+            array.push(node);
+            continue;
+        }
+        
+        array.push(...node.children);
+        counters[counters.length - 1]++;
+    }
+
+    return counters;
+}
 
 module.exports = levelWidth;
