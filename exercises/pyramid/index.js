8d965b328ace4a6c9ec877a11fb98b18237f630a
max <mkondzera@talend.com>
2019-10-17 18:25:27 +0300

    implement binary search tree node

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,30 +14,30 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n, row = 0, level = '') {
-    if (n === row) {
-        return;
+// Iterative solution O(n)
+const pyramid = n => {
+    for (let i = 1, j = 1; i <= n; i++, j += 2) {
+        console.log(`${' '.repeat(n - i)}${'#'.repeat(j)}${' '.repeat(n - i)}`)
     }
-
-    if (level.length === 2 * n - 1) {
-        console.log(level);
-        return pyramid(n, row + 1);
-    }
-
-    const midpoint = Math.floor((2 * n - 1) / 2);
-    const add = (midpoint - row <= level.length && midpoint + row >= level.length) ? '#' : ' ';
-
-    return pyramid(n, row, level + add);
 }
 
-module.exports = pyramid;
-
-// Iterative solution O(n)
-// const pyramid = n => {
-//     for (let i = 1, j = 1; i <= n; i++, j += 2) {
-//         console.log(`${' '.repeat(n - i)}${'#'.repeat(j)}${' '.repeat(n - i)}`)
+// function pyramid(n, row = 0, level = '') {
+//     if (n === row) {
+//         return;
 //     }
+
+//     if (level.length === 2 * n - 1) {
+//         console.log(level);
+//         return pyramid(n, row + 1);
+//     }
+
+//     const midpoint = Math.floor((2 * n - 1) / 2);
+//     const add = (midpoint - row <= level.length && midpoint + row >= level.length) ? '#' : ' ';
+
+//     return pyramid(n, row, level + add);
 // }
+
+module.exports = pyramid;
 
 // Iterative solution O(n ^ 2)
 // function pyramid(n) {
