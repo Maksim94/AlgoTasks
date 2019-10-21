62bedb400b3091d5985072faab39db2e908abc29
max <mkondzera@talend.com>
2019-10-21 16:57:23 +0300

    implement buble sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,7 +2,21 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
+    let isSorted = false;
+    let lastUnsorted = arr.length - 1;
 
+    while(!isSorted) {
+        isSorted = true;
+        for(let j = 0; j < lastUnsorted; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+                isSorted = false;
+            }
+        }
+        lastUnsorted--;
+    }
+
+    return arr;
 }
 
 function selectionSort(arr) {
