dade65c537f83d9d67a80de92f14382eba433a1e
max <mkondzera@talend.com>
2019-11-26 16:54:36 +0200

    implement sorting algorithms

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -20,15 +20,54 @@ function bubbleSort(arr) {
 }
 
 function selectionSort(arr) {
+    const length = arr.length;
 
+    for (let i = 0; i < length; i++) {
+        indexOfMin = i;
+
+        for (let j = i + 1; j < length; j++) {
+            if (arr[j] < arr[indexOfMin]) {
+                indexOfMin = j;
+            }
+        }
+
+        if (i !== indexOfMin) {
+            const lesser = arr[indexOfMin];
+            arr[indexOfMin] = arr[i];
+            arr[i] = lesser;
+        }
+    }
+
+    return arr;
 }
 
 function mergeSort(arr) {
+    if (arr.length === 1) {
+        return arr;
+    }
 
+    const midPoint = Math.floor(arr.length / 2);
+    const left = arr.slice(0, midPoint);
+    const right = arr.slice(midPoint);
+
+    return merge(
+        mergeSort(left),
+        mergeSort(right)
+    );
 }
 
 function merge(left, right) {
+    const results = [];
 
+    while(left.length && right.length) {
+        if (left[0] < right[0]) {
+            results.push(left.shift());
+        } else {
+            results.push(right.shift());
+        }
+    }
+
+    return [...results, ...left, ...right];
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
